84c6c48bc291bfe5d7b307d1d9fb5874688d5c5e
samizoda8 <167873586+samizoda8@users.noreply.github.com>
2024-07-25 17:36:39 +0500

    Update settings.py

--- djcms/settings.py
+++ djcms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = environ.get('SECRET_KEY', 'dev-bittu@githib.com/dev-bittu#djcms')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
