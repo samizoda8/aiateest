04a7f79568ea1ab82e8003054af9b1d345cc7d36
samizoda8 <167873586+samizoda8@users.noreply.github.com>
2024-07-25 18:38:41 +0500

    Update urls.py

--- djcms/urls.py
+++ djcms/urls.py
@@ -21,19 +21,19 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('blog/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
-    path("blog/accounts/", include("accounts.urls")),
-    path("blog/blogs/", include("blogs.urls")),
-    path("blog/manage/", include("management.urls")),
+    path("accounts/", include("accounts.urls")),
+    path("blogs/", include("blogs.urls")),
+    path("manage/", include("management.urls")),
 
-    path("blog/", views.Index.as_view(), name="index"),
-    path("blog/trendings/", views.Trendings.as_view(), name="trendings"),
-    path("blog/latest/", views.Latest.as_view(), name="latest"),
-    path("blog/popular/", views.Popular.as_view(), name="popular"),
-    path("blog/search/", views.Search.as_view(), name="search"),
-    path("blog/bookmark/", views.BookmarkView.as_view(), name="bookmark"),
-    path("blog/terms-and-conditions/", views.TermsAndConditions.as_view(), name="terms_and_conditions"),
-    path("blog/category/", views.CategoryView.as_view(), name="category"),
-    path("blog/category/<slug:cat>/", views.GetCategory.as_view(), name="get_category"),
+    path("", views.Index.as_view(), name="index"),
+    path("trendings/", views.Trendings.as_view(), name="trendings"),
+    path("latest/", views.Latest.as_view(), name="latest"),
+    path("popular/", views.Popular.as_view(), name="popular"),
+    path("search/", views.Search.as_view(), name="search"),
+    path("bookmark/", views.BookmarkView.as_view(), name="bookmark"),
+    path("terms-and-conditions/", views.TermsAndConditions.as_view(), name="terms_and_conditions"),
+    path("category/", views.CategoryView.as_view(), name="category"),
+    path("category/<slug:cat>/", views.GetCategory.as_view(), name="get_category"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
